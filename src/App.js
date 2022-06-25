70def4790fa23ed831fa3e8d70347a7c9b4de8eb
ardhanata <g.d.ardhanata@gmail.com>
2022-06-25 15:00:26 +0700

    tes 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
+import Header from './components/Header';
+import Contact from './components/Contact';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  
+return (
+  <div className="App" style={{ display: "flex", 'flexDirection': "column" }}>
+    <Header />
+    {contacts.map((contact) => {
+      return <Contact data={contact}>
+      </Contact>
+    })}
+  </div>
+);
 }
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,21 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = ({ data }) => {
+    const { name, email, phone, photo } = data;
     return (
-        <></>
-    )
-}
+      <>
+        <div key="email" style={{ display: "flex", flexDirection: "row" }}>
+          <img src={photo} width="50px" alt={name} />
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <p>{name}</p>
+            <p>{phone}</p>
+            <p>{email}</p>
+          </div>
+        </div>
+        <br />
+      </>
+    );
+  };
 
 export default Contact;
